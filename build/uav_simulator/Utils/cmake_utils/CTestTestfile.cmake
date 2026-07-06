2239f4da6aeeff741feedbbc64fe5416605fce35
Guoyulun <guoyulun@users.noreply.github.com>
2026-07-06 19:55:36 +0800

    chore: 忽略编译产物和生成文件

--- build/uav_simulator/Utils/cmake_utils/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zhuyihang/SimEnv/src/uav_simulator/Utils/cmake_utils
-# Build directory: /home/zhuyihang/SimEnv/build/uav_simulator/Utils/cmake_utils
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- devel/share/building_generator_interfaces/cmake/building_generator_interfaces-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(building_generator_interfaces_MESSAGE_FILES "")
-set(building_generator_interfaces_SERVICE_FILES "/home/zhuyihang/SimEnv/src/building_generator_interfaces/srv/CallElevator.srv;/home/zhuyihang/SimEnv/src/building_generator_interfaces/srv/SetDoorState.srv")
