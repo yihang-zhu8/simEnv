a2d03d61f8366280999f2cb5245c92edc8ced9c2
bxcswsw <3465281648@qq.com>
2026-07-08 18:16:47 +0800

    11

--- build/devel/share/unitree_guide/cmake/unitree_guide-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(unitree_guide_MESSAGE_FILES "/home/ros/Guoyulun/Competition/SimEnv/src/unitree_guide/unitree_guide/unitree_guide/msg/CustomPoint.msg;/home/ros/Guoyulun/Competition/SimEnv/src/unitree_guide/unitree_guide/unitree_guide/msg/CustomMsg.msg")
-set(unitree_guide_SERVICE_FILES "")
